927d38ece626aaf714b7a0f8c54710a81479ff3d
Logan Livingston <lrlivingston@live.com>
2020-07-31 13:17:50 -0500

    env variables

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Episode } from './episodes/episode.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EpisodesController } from './episodes/episodes.controller';
 import { EpisodesModule } from './episodes/episodes.module';
 import { Module } from '@nestjs/common';
 
@@ -9,14 +8,14 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "root",
-    "password": "root",
-    "database": "test",
-    "entities": [Episode],
-    "synchronize": true
+    type: 'mysql',
+    host: process.env.HOST || 'localhost',
+    port: 3306,
+    username: process.env.USERNAME || 'root',
+    password: process.env.PASSWORD || 'root',
+    database: process.env.DB || 'test',
+    entities: [Episode],
+    synchronize: true
   }), EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
