70d2e6381674c67fc9a571f18223f01cdf32ee41
Logan Livingston <lrlivingston@live.com>
2020-08-04 23:02:49 -0500

    [LL, MB,CG] build plugin for scully
    Co-authored-by: steamcleaner <1mattgt2@gmail.com>
    Co-authored-by: cameron-jf-gray <cameron.jf.gray@gmail.com>

--- plugins/netlify-plugin-scully-build.js
+++ plugins/netlify-plugin-scully-build.js
@@ -1,4 +1,32 @@
 const axios = require('axios');
+var glob = require('glob');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+function processFile(file) {
+  return new Promise((_resolve, _reject) => {
+    fs.readFile(file, (err, contents) => {
+      const body = '' + contents;
+      const filePieces = file.split('/');
+      const fileName = filePieces.pop();
+      const id = filePieces.pop();
+      if (!JSON.parse(body)[`episode${id}`]) {
+        rimraf.sync(`public/episode/${id}`);
+      }
+      _resolve();
+    });
+  });
+}
+
+function cleanup() {
+  return new Promise((resolve, reject) => {
+    glob('public/episode/**/*.json', {}, async (error, files) => {
+      const filePromises = files.map((file) => processFile(file));
+      await Promise.all(filePromises);
+      resolve();
+    });
+  });
+}
 
 module.exports = {
   onPreBuild: async ({ inputs }) => {
@@ -12,5 +40,8 @@ module.exports = {
     await axios.get(
       `https://hogcast-services.herokuapp.com/api/episodes/reset-changes`
     );
+  },
+  onSuccess: async () => {
+    await cleanup();
   },
 };
